3cba3902f105acc0ed8924a8c98e2b1ca5626538
jamesmoore255 <email_id>
2020-01-23 22:37:16 +1030

    add the json web token helper and crypto random string util

---
+++ lib/helpers/jwt.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+/// Parse the Javascript Web Token
+///
+/// [token] The javascript web token.
+Map<String, dynamic> parseJwt(String token) {
+  final parts = token.split('.');
+  if (parts.length != 3) {
+    throw Exception('invalid token');
+  }
+
+  final payload = _decodeBase64(parts[1]);
+  final payloadMap = json.decode(payload);
+  if (payloadMap is! Map<String, dynamic>) {
+    throw Exception('invalid payload');
+  }
+
+  return payloadMap;
+}
+
+/// Decode a base 64 encoded string
+String _decodeBase64(String str) {
+  String output = str.replaceAll('-', '+').replaceAll('_', '/');
+
+  switch (output.length % 4) {
+    case 0:
+      break;
+    case 2:
+      output += '==';
+      break;
+    case 3:
+      output += '=';
+      break;
+    default:
+      throw Exception('Illegal base64url string!"');
+  }
+
+  return utf8.decode(base64Url.decode(output));
+}
