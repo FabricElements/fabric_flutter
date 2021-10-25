8f921ab1a46c76ca8d2a8f645770f09fda749fa3
Erny Sans <ernysans@icloud.com>
2021-10-25 14:51:32 -0500

    Navigation breadcrumbs

--- lib/component.dart
+++ lib/component.dart
@@ -1,12 +1,13 @@
+export 'component/breadcrumbs.dart';
 export 'component/card_button.dart';
 export 'component/charts.dart';
 export 'component/flag_chip.dart';
 export 'component/global_providers.dart';
 export 'component/language_selector.dart';
-export 'component/smart_button.dart';
 export 'component/preview_audio.dart';
 export 'component/role_selector.dart';
 export 'component/section_title.dart';
+export 'component/smart_button.dart';
 export 'component/smart_image.dart';
 export 'component/status_chip.dart';
 export 'component/top_app.dart';
---
+++ lib/component/breadcrumbs.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import '../helper/options.dart';
+
+/// Navigation [Breadcrumbs] provides a useful way to display navigation routes
+class Breadcrumbs extends StatelessWidget {
+  const Breadcrumbs({
+    Key? key,
+    required this.buttons,
+    this.buttonStyle,
+    this.dividerStyle,
+    this.padding = const EdgeInsets.all(0),
+    this.spacing = 8,
+    this.textStyle,
+  }) : super(key: key);
+
+  final List<ButtonOptions> buttons;
+  final ButtonStyle? buttonStyle;
+  final TextStyle? dividerStyle;
+  final EdgeInsetsGeometry padding;
+  final double spacing;
+  final TextStyle? textStyle;
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    TextTheme textTheme = theme.textTheme;
+    List<Widget> items = [];
+    TextStyle? _textStyle = textStyle ?? textTheme.caption;
+    TextStyle? _dividerStyle = dividerStyle ?? textTheme.caption;
+    for (int i = 0; i < buttons.length; i++) {
+      ButtonOptions button = buttons[i];
+      bool clickable = button.path != null || button.onTap != null;
+      items.add(
+        TextButton(
+          onPressed: !clickable ? null : () {
+            if (button.onTap != null) button.onTap!();
+            if (button.path != null) {
+              Navigator.of(context).popAndPushNamed(button.path!);
+            }
+          },
+          child: Text(button.label, style: _textStyle),
+          style: buttonStyle,
+        ),
+      );
+      if (i < (buttons.length - 1)) {
+        items.add(Text("/", style: _dividerStyle));
+      }
+    }
+    return Padding(
+      padding: padding,
+      child: Wrap(
+        spacing: spacing,
+        runSpacing: spacing,
+        children: items,
+        crossAxisAlignment: WrapCrossAlignment.center,
+      ),
+    );
+  }
+}
