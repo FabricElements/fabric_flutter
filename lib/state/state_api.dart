d13166b841710d6a87ac94063a36519a08355851
Erny Sans <ernysans@icloud.com>
2022-03-10 14:06:53 -0500

    StateAPI: User Token authentication and endpoint get request fix

--- lib/state/state_api.dart
+++ lib/state/state_api.dart
@@ -1,9 +1,13 @@
 import 'dart:convert';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
+import '../helper/http_request.dart';
 import 'state_shared.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
 
 /// Base State for API calls
 /// Use this state to fetch updated data every time an endpoint is updated
@@ -28,15 +32,21 @@ class StateAPI extends ChangeNotifier with StateShared {
   /// [authScheme]
   /// https://developer.mozilla.org/en-US/docs/Web/HTTP/Authentication#authentication_schemes
   /// Use Bearer for token authentication
-  String? authScheme;
+  AuthScheme? authScheme;
+
+  /// [token]
+  /// Use [AuthScheme.Bearer] and the current user id token for authentication
+  bool token = false;
+
+  bool loading = false;
 
   /// Clear and reset default values
   void clear({bool notify = false}) {
     _errorCount = 0;
     data = null;
-    baseEndpoint = null;
+    // baseEndpoint = null;
     initialized = false;
-    _lastEndpointCalled = null;
+    // _lastEndpointCalled = null;
     clearAfter();
     if (notify) notifyListeners();
   }
@@ -46,8 +56,8 @@ class StateAPI extends ChangeNotifier with StateShared {
   /// Don't use a '/' at the beginning of the path
   set endpoint(String? value) {
     if (value == baseEndpoint && data != null) return;
-    if (value != baseEndpoint) clear(notify: false);
-    if (initialized) return;
+    // if (value != baseEndpoint) clear(notify: false);
+    // if (initialized) return;
     baseEndpoint = value;
     if (_errorCount > 1) {
       if (kDebugMode) {
@@ -61,40 +71,60 @@ class StateAPI extends ChangeNotifier with StateShared {
 
   /// API Call
   void get({bool ignoreDuplicatedCalls = false}) async {
+    if (loading) return;
+    loading = true;
+    if (baseEndpoint == null) {
+      data = null;
+      error = 'endpoint can\'t be null';
+      _errorCount++;
+      loading = false;
+      notifyListeners();
+      return;
+    }
+    if (ignoreDuplicatedCalls &&
+        _lastEndpointCalled != null &&
+        _lastEndpointCalled == baseEndpoint) {
+      loading = false;
+      return;
+    }
+    if (_lastEndpointCalled != baseEndpoint) {
+      _errorCount = 0;
+      data = null;
+      initialized = false;
+    }
+    _lastEndpointCalled = baseEndpoint;
     String? _error;
     if (_errorCount > 1) {
       if (kDebugMode) {
         print('$_errorCount errors calls to endpoint: $baseEndpoint');
       }
-      return;
-    }
-    if (baseEndpoint == null) {
-      data = null;
-      error = 'endpoint can\'t be null';
-      _errorCount++;
-      notifyListeners();
+      loading = false;
       return;
     }
     bool mustAuthenticate = false;
     bool canAuthenticate = false;
+    if (token) {
+      authScheme = AuthScheme.Bearer;
+      credentials = await _auth.currentUser?.getIdToken();
+    }
     if (authScheme != null || credentials != null) {
       mustAuthenticate = true;
       canAuthenticate = authScheme != null && credentials != null;
     }
     if (mustAuthenticate && !canAuthenticate) {
       if (kDebugMode) print('Must Authenticate on call: $baseEndpoint');
+      loading = false;
       return;
     }
     bool willAuthenticate = mustAuthenticate && canAuthenticate;
-    if (ignoreDuplicatedCalls && _lastEndpointCalled == baseEndpoint) return;
-    initialized = true;
-    _lastEndpointCalled = baseEndpoint;
-    if (kDebugMode) print('Calling endpoint: $baseEndpoint');
     Uri url = Uri.parse(baseEndpoint!);
     Map<String, String> headers = {};
     if (willAuthenticate) {
-      headers.addAll({'Authorization': '$authScheme $credentials'});
+      headers.addAll({
+        'Authorization': '${describeEnum(authScheme!)} $credentials',
+      });
     }
+    if (kDebugMode) print('Calling endpoint: $baseEndpoint');
     final response = await http.get(url, headers: headers);
     if (response.statusCode == 200) {
       try {
@@ -105,9 +135,9 @@ class StateAPI extends ChangeNotifier with StateShared {
       }
     } else {
       _error =
-          response.reasonPhrase != null && response.reasonPhrase!.isNotEmpty
-              ? response.reasonPhrase
-              : null;
+      response.reasonPhrase != null && response.reasonPhrase!.isNotEmpty
+          ? response.reasonPhrase
+          : null;
       if (error == null) {
         _error = 'error--${response.statusCode}';
       }
@@ -118,6 +148,8 @@ class StateAPI extends ChangeNotifier with StateShared {
       error = _error;
       data = null;
     }
+    initialized = true;
+    loading = false;
     notifyListeners();
   }
 }
