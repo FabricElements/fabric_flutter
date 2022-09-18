d313954397b10b6382f372e2177f79dcf3e27943
Erny Sans <ernysans@icloud.com>
2022-09-18 07:54:32 -0500

    UpdatePassword component

--- lib/helper/regex_helper.dart
+++ lib/helper/regex_helper.dart
@@ -4,4 +4,13 @@ class RegexHelper {
   static final phone = RegExp(r'^\+\d{8,15}');
   static final url = RegExp(
       r'(http|https)(://)[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)');
+
+  /// Password regex
+  /// At least one upper case English letter, (?=.*?[A-Z])
+  /// At least one lower case English letter, (?=.*?[a-z])
+  /// At least one digit, (?=.*?[0-9])
+  /// At least one special character, (?=.*?[#?!@$%^&*-])
+  /// Minimum eight in length .{8,} (with the anchors)
+  static final password =
+      RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$');
 }
---
+++ lib/serialized/password_data.dart
@@ -0,0 +1,19 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'password_data.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+class PasswordData {
+  String currentPassword;
+  String newPassword;
+
+  PasswordData({
+    required this.currentPassword,
+    required this.newPassword,
+  });
+
+  factory PasswordData.fromJson(Map<String, dynamic>? json) =>
+      _$PasswordDataFromJson(json ?? {});
+
+  Map<String, dynamic> toJson() => _$PasswordDataToJson(this);
+}
