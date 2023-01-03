d5a748bb352829741ba409c4b476166f0257a4cb
Erny Sans <ernysans@icloud.com>
2023-01-03 07:40:02 -0500

    ButtonOptions: Image support

--- lib/helper/options.dart
+++ lib/helper/options.dart
@@ -2,57 +2,61 @@ import 'package:flutter/widgets.dart';
 
 /// Predefined options for internal components
 
-/// [ButtonOptions]
+/// ButtonOptions
 class ButtonOptions {
-  /// [icon]
+  /// icon
   IconData? icon;
 
-  /// Define [id]
+  /// Define id
   String? id;
 
-  /// Define [label] text for the button
+  /// Define label text for the button
   String label;
 
-  /// Define [labelAlt] alt text for the button
+  /// Define labelAlt alt text for the button
   String? labelAlt;
 
-  /// [onTap] button
+  /// onTap button
   Function? onTap;
 
-  /// [path] to redirect
+  /// path to redirect
   String? path;
 
   /// QueryParameters to use with path
   Map<String, List<String>>? queryParameters;
 
-  /// set [pop] to `true` to use Navigator.popAndPushNamed
+  /// set pop to `true` to use Navigator.popAndPushNamed
   bool pop;
 
-  /// set [important] to `true` to use custom design or functionality
+  /// set important to `true` to use custom design or functionality
   bool important;
 
-  /// set [selected] to `true` to use custom design or functionality
+  /// set selected to `true` to use custom design or functionality
   /// Don't use as final in case you need to update it's value programmatically
   bool selected;
 
-  /// set [value] as dynamic and cast `value as String` or any type required
+  /// set value as dynamic and cast `value as String` or any type required
   dynamic value;
 
-  /// Set [children] for submenus
+  /// Set children[] for submenus
   List<ButtonOptions> children;
+
+  /// Set image for custom buttons
+  String? image;
 
   ButtonOptions({
     this.children = const [],
     this.icon,
     this.id,
+    this.image,
     this.important = false,
     this.label = '',
     this.labelAlt,
     this.onTap,
     this.path,
     this.pop = false,
+    this.queryParameters,
     this.selected = false,
     this.value,
-    this.queryParameters,
   });
 }
