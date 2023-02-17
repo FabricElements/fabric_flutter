1bdefbf131b47e393c7a4f6d4a34b3e9d2fc4252
Erny Sans <ernysans@icloud.com>
2023-02-17 07:11:01 -0500

    FLagChip: Layout

--- lib/component/flag_chip.dart
+++ lib/component/flag_chip.dart
@@ -30,49 +30,28 @@ class FlagChip extends StatelessWidget {
   Widget build(BuildContext context) {
     final NumberFormat formatDecimal = NumberFormat.decimalPattern();
     final theme = Theme.of(context);
-    final TextTheme textTheme = theme.textTheme;
-
     List<Widget> items = [];
     late Widget icon;
 
     if (language == 'total') {
       icon = const Icon(Icons.info);
     } else {
-      icon = Text(
-        ISOLanguages.getEmoji(language) ?? '',
-        style: textTheme.bodyText2!.copyWith(
-          fontSize: 20,
-        ),
-        textAlign: TextAlign.center,
-      );
+      icon = Text(ISOLanguages.getEmoji(language) ?? '');
     }
     items.add(
       Padding(
         padding: const EdgeInsets.only(right: 8),
-        child: Text(
-          language.toUpperCase(),
-          style: textTheme.bodyText2,
-        ),
+        child: Text(language.toUpperCase()),
       ),
     );
     if (total != null) {
-      items.add(
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 4),
-          child: Material(
-            color: theme.colorScheme.primaryContainer,
-            clipBehavior: Clip.hardEdge,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(4),
-            ),
-            elevation: 0,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
-              child: Text(formatDecimal.format(total)),
-            ),
-          ),
+      items.add(Text(
+        formatDecimal.format(total),
+        style: TextStyle(
+          color: theme.colorScheme.primary,
+          fontWeight: FontWeight.bold,
         ),
-      );
+      ));
     }
 
     return Chip(
@@ -86,29 +65,5 @@ class FlagChip extends StatelessWidget {
       ),
       onDeleted: onDeleted,
     );
-
-    // return Padding(
-    //   padding: const EdgeInsets.symmetric(vertical: 8),
-    //   child: ConstrainedBox(
-    //     constraints: const BoxConstraints(minWidth: 10, minHeight: 10),
-    //     child: Material(
-    //       color: color,
-    //       clipBehavior: Clip.hardEdge,
-    //       shape: RoundedRectangleBorder(
-    //         borderRadius: BorderRadius.circular(16.0),
-    //       ),
-    //       elevation: 0,
-    //       child: Center(
-    //         widthFactor: 1.0,
-    //         heightFactor: 1.0,
-    //         child: Wrap(
-    //           crossAxisAlignment: WrapCrossAlignment.center,
-    //           direction: Axis.horizontal,
-    //           children: items,
-    //         ),
-    //       ),
-    //     ),
-    //   ),
-    // );
   }
 }
