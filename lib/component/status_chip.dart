9ef3716b1a6d4177408abb44ee0d501bab129c90
Erny Sans <ernysans@icloud.com>
2023-04-03 06:04:38 -0500

    Status Chip: Icons

--- lib/component/status_chip.dart
+++ lib/component/status_chip.dart
@@ -22,32 +22,49 @@ class StatusChip extends StatelessWidget {
     Color statusColor = Colors.grey.shade800;
     String baseStatus = status ?? 'unknown';
     baseStatus = locales.get('label--$status');
+    IconData icon = Icons.circle;
 
     switch (status) {
       case 'draft':
-        statusColor = Colors.indigo.shade500;
+        statusColor = Colors.blueGrey.shade600;
+        icon = Icons.circle;
         break;
       case 'review':
         statusColor = Colors.amber.shade900;
+        icon = Icons.remove_red_eye;
         break;
       case 'approved':
         statusColor = Colors.deepPurple.shade500;
+        icon = Icons.check_circle;
         break;
       case 'rejected':
+        icon = Icons.warning;
         statusColor = Colors.red.shade500;
         break;
       case 'inactive':
+        icon = Icons.warning;
+        statusColor = Colors.red.shade500;
+        break;
+      case 'paused':
+        icon = Icons.pause_circle;
         statusColor = Colors.deepOrange.shade500;
         break;
+      case 'scheduled':
+        icon = Icons.schedule;
+        statusColor = Colors.deepPurple.shade500;
+        break;
       case 'active':
-        statusColor = Colors.green.shade600;
+        icon = Icons.incomplete_circle;
+        statusColor = Colors.teal.shade600;
         break;
       case 'archived':
+        icon = Icons.archive;
         statusColor = Colors.grey.shade700;
     }
     return Tooltip(
       message: locales.get('label--status'),
       child: Chip(
+        avatar: Icon(icon, color: Colors.white),
         label: Text(
           baseStatus,
           style: const TextStyle(
