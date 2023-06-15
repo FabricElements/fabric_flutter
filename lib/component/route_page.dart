6f7779866a03765318d98fee4fb64611cc71c7ee
Erny Sans <ernysans@icloud.com>
2023-06-15 18:35:59 -0500

    RoutePage: Added loading widget when data is null

--- lib/component/route_page.dart
+++ lib/component/route_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../helper/route_helper.dart';
+import '../placeholder/loading_screen.dart';
 import '../serialized/user_status.dart';
 import '../state/state_alert.dart';
 
@@ -12,11 +13,13 @@ class RoutePage extends StatelessWidget {
     required this.uri,
     required this.stream,
     required this.status,
+    this.loading = const LoadingScreen(),
   }) : super(key: key);
   final RouteHelper routeHelper;
   final Uri uri;
-  final Stream<UserStatus> stream;
-  final UserStatus status;
+  final Stream<UserStatus>? stream;
+  final UserStatus? status;
+  final Widget loading;
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +29,7 @@ class RoutePage extends StatelessWidget {
       initialData: status,
       stream: stream,
       builder: (context, snapshot) {
+        if (snapshot.data == null) return loading;
         UserStatus userStatus = snapshot.data as UserStatus;
         Map<String, Widget> routes = routeHelper.routes(
           signed: userStatus.signedIn,
