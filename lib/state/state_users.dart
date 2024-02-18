7ac475a08db079dd2ed015e7a0e5abd63372f798
Erny Sans <ernysans@icloud.com>
2024-02-18 09:01:31 -0500

    Update StateUser default limit to 100

--- lib/state/state_users.dart
+++ lib/state/state_users.dart
@@ -3,7 +3,7 @@ import 'state_collection.dart';
 
 class StateUsers extends StateCollection {
   @override
-  int limitDefault = 50;
+  int limitDefault = 100;
 
   @override
   List<UserData> get serialized {
