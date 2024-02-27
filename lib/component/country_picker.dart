6eda7c15c72a1b1cf117990801f085aafd4a22d5
Erny Sans <ernysans@users.noreply.github.com>
2024-02-27 14:53:09 -0500

    Language and Country pickers with updated data and serialization (#91)
    * Language and Country pickers with updated data and serialization
    
    * Version update
    
    * Dependency and version update

--- lib/component/country_picker.dart
+++ lib/component/country_picker.dart
@@ -1,68 +1,66 @@
 import 'package:flutter/material.dart';
 
 import '../helper/app_localizations_delegate.dart';
-import '../helper/iso_language.dart';
+import '../helper/iso_countries.dart';
 import '../helper/options.dart';
 import '../serialized/iso_data.dart';
 import 'input_data.dart';
 
-/// A Picker used to select a language by it's alpha2 code
+/// A Picker used to select a country by it's alpha2 code
 ///
-/// [voice] Dictates whether the campaign is using voice calls or text messages.
-/// [value] This is the language alpha2 code, it will default to english.
+/// [value] This is the country alpha2 code, it will default to us.
 /// [onChange] This will push selected language iso to parent widget to sync with campaign data.
 /// ```dart
-/// LanguagePicker(
-///   voice: false,
-///   value: 'es',
+/// CountryPicker(
+///   value: 'US',
 ///   onChange: (String iso) {
-///     selectedLanguage = iso;
+///     selectedCountry = iso;
 ///   }
 /// );
 /// ```
-class LanguagePicker extends StatelessWidget {
-  const LanguagePicker({
+class CountryPicker extends StatelessWidget {
+  const CountryPicker({
     super.key,
-    this.voice = false,
-    this.value = 'en',
+    this.value = 'US',
+    this.phoneNumberOrigin = false,
     this.label,
     this.hintText,
     this.disabled = false,
     required this.onChange,
   });
 
-  final bool voice;
   final String? value;
   final Function(String?) onChange;
   final String? hintText;
   final String? label;
   final bool disabled;
+  final bool phoneNumberOrigin;
 
   @override
   Widget build(BuildContext context) {
     final locales = AppLocalizations.of(context);
-    List<ISOLanguage> items = ISOLanguages.languages;
-    if (voice) {
+    List<ISOCountry> items = ISOCountries.countries;
+    if (phoneNumberOrigin) {
       // Filter available voices by those available on WaveNet
       items = items.where((element) {
-        return ISOLanguages.waveNetLanguages.contains(element.alpha2);
+        return ISOCountries.phoneSupportedCountries.contains(element.alpha2);
       }).toList();
     }
     // List of iso's corresponding to the text widgets
     List<ButtonOptions> options = List.generate(items.length, (index) {
       final item = items[index];
       return ButtonOptions(
-        label: '${item.emoji} ${item.name} (${item.alpha2})',
-        labelAlt: item.nativeName,
+        label: '${item.flag} ${item.name} (${item.alpha2})',
+        labelAlt: item.fullName,
         value: item.alpha2,
       );
     });
     return InputData(
-      prefixIcon: const Icon(Icons.language),
-      label: label ?? locales.get('label--language'),
+      prefixIcon: const Icon(Icons.flag),
+      label: label ?? locales.get('label--country'),
       hintText: hintText ??
           locales.get(
-              'label--choose-label', {'label': locales.get('label--language')}),
+              'label--choose-label', {'label': locales.get('label--country')}),
       value: value,
       type: InputDataType.dropdown,
       options: options,
--- lib/serialized/iso_data.g.dart
+++ lib/serialized/iso_data.g.dart
@@ -22,3 +22,42 @@ Map<String, dynamic> _$ISOLanguageToJson(ISOLanguage instance) =>
       'nativeName': instance.nativeName,
       'emoji': instance.emoji,
     };
+
+ISOCountry _$ISOCountryFromJson(Map<String, dynamic> json) => ISOCountry(
+      capital: json['capital'] as String?,
+      citizenship: json['citizenship'] as String?,
+      countryCode: json['countryCode'] as String,
+      currency: json['currency'] as String?,
+      currencyCode: json['currencyCode'] as String?,
+      currencySubUnit: json['currencySubUnit'] as String?,
+      fullName: json['fullName'] as String?,
+      alpha2: json['alpha2'] as String,
+      alpha3: json['alpha3'] as String,
+      name: json['name'] as String,
+      regionCode: json['regionCode'] as String?,
+      subRegionCode: json['subRegionCode'] as String?,
+      eea: json['eea'] as bool? ?? false,
+      callingCode: json['callingCode'] as String?,
+      currencySymbol: json['currencySymbol'] as String?,
+      flag: json['flag'] as String? ?? '🌐',
+    );
+
+Map<String, dynamic> _$ISOCountryToJson(ISOCountry instance) =>
+    <String, dynamic>{
+      'capital': instance.capital,
+      'citizenship': instance.citizenship,
+      'countryCode': instance.countryCode,
+      'currency': instance.currency,
+      'currencyCode': instance.currencyCode,
+      'currencySubUnit': instance.currencySubUnit,
+      'fullName': instance.fullName,
+      'alpha2': instance.alpha2,
+      'alpha3': instance.alpha3,
+      'name': instance.name,
+      'regionCode': instance.regionCode,
+      'subRegionCode': instance.subRegionCode,
+      'eea': instance.eea,
+      'callingCode': instance.callingCode,
+      'currencySymbol': instance.currencySymbol,
+      'flag': instance.flag,
+    };
