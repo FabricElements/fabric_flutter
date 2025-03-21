0511c5900717e94fa419cb7259794653a0a51c2e
Erny Sans <ernysans@icloud.com>
2025-03-20 22:26:20 -0500

    Add onDeleted and avatarPrefix properties to UserChip widget

--- lib/component/user_chip.dart
+++ lib/component/user_chip.dart
@@ -11,11 +11,15 @@ class UserChip extends StatefulWidget {
     required this.uid,
     this.minimal = false,
     this.labelStyle,
+    this.onDeleted,
+    this.avatarPrefix,
   });
 
   final String? uid;
   final bool minimal;
   final TextStyle? labelStyle;
+  final Function()? onDeleted;
+  final String? avatarPrefix;
 
   @override
   State<UserChip> createState() => _UserChipState();
@@ -25,21 +29,26 @@ class _UserChipState extends State<UserChip> {
   @override
   Widget build(BuildContext context) {
     if (widget.uid == null) return const SizedBox(width: 0, height: 0);
-    final stateUsers = Provider.of<StateUsers>(context, listen: false);
+    final stateUsers = Provider.of<StateUsers>(context, listen: true);
     final user = stateUsers.getUser(widget.uid!);
     String label = user.id!;
     if (user.username != null) label = user.username!;
     if (user.name.isNotEmpty) label = user.name;
     if (widget.minimal) return Text(label, style: widget.labelStyle);
+    String? url = user.avatar;
+    if (widget.avatarPrefix != null && user.avatar != null) {
+      url = '${widget.avatarPrefix}/${user.avatar}';
+    }
     return Chip(
       avatar: UserAvatar(
-        avatar: user.avatar,
+        avatar: url,
         firstName: user.firstName,
         lastName: user.lastName,
         name: user.name,
         presence: user.presence,
       ),
       label: Text(label),
+      onDeleted: widget.onDeleted,
     );
   }
 }
