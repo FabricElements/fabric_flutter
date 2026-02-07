654fd2396bb2007fd056687c65cf1e811000bfb2
Erny Sans <ernysans@icloud.com>
2026-02-07 16:14:30 -0500

    Refactor loading_screen: simplify theme handling and enhance loading indicator layout

--- lib/placeholder/loading_screen.dart
+++ lib/placeholder/loading_screen.dart
@@ -13,39 +13,40 @@ class LoadingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
     // Use system theme colors
-    final brightness = MediaQuery.of(context).platformBrightness;
-    if (brightness == Brightness.dark) {
-      theme.copyWith(colorScheme: ThemeData.dark().colorScheme);
-    } else {
-      theme.copyWith(colorScheme: ThemeData.light().colorScheme);
-    }
+    // final brightness = MediaQuery.of(context).platformBrightness;
+    // if (brightness == Brightness.dark) {
+    //   print('Using dark theme');
+    //   theme = theme.copyWith(colorScheme: ThemeData.dark().colorScheme);
+    // } else {
+    //   print('Using light theme');
+    //   theme = theme.copyWith(colorScheme: ThemeData.light().colorScheme);
+    // }
+    // print(theme.colorScheme.surface);
+    // print('--------------------------------');
 
     if (log) {
       Utils.getParentWidgetName(context);
     }
 
-    return Theme(
-      data: theme,
-      child: Container(
-        color: theme.scaffoldBackgroundColor,
-        height: double.maxFinite,
-        width: double.maxFinite,
-        child: Column(
-          children: [
-            if (parent) AppBar(),
-            Spacer(),
-            SizedBox(
-              width: kToolbarHeight,
-              height: kToolbarHeight,
-              child: CircularProgressIndicator.adaptive(
-                valueColor: AlwaysStoppedAnimation<Color>(
-                  theme.colorScheme.primary,
-                ),
+    return Container(
+      color: theme.colorScheme.surface,
+      height: double.maxFinite,
+      width: double.maxFinite,
+      child: Column(
+        children: [
+          if (parent) AppBar(),
+          Spacer(),
+          SizedBox(
+            width: kToolbarHeight,
+            height: kToolbarHeight,
+            child: CircularProgressIndicator.adaptive(
+              valueColor: AlwaysStoppedAnimation<Color>(
+                theme.colorScheme.primary,
               ),
             ),
-            Spacer(),
-          ],
-        ),
+          ),
+          Spacer(),
+        ],
       ),
     );
   }
