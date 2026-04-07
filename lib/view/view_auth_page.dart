e6373fdaf1bc435f95520e5310af97ebfe1e464f
Erny Sans <ernysans@icloud.com>
2026-04-07 11:29:36 -0500

    Update dependencies for flutter_markdown_plus and related packages

--- lib/view/view_auth_page.dart
+++ lib/view/view_auth_page.dart
@@ -5,7 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
 
